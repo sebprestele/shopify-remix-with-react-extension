e05ea2a2772f8f3c1e20eb1633c115b31636106d
Sebastian Prestele <sebastian.prestele@gmail.com>
2024-01-24 18:11:10 +0100

    chore(setup): Setup App with Remix and React frontend

---
+++ frontend/components/App.jsx
@@ -0,0 +1,12 @@
+import Button from "./Button"
+
+export default function App({ home }) {
+  console.log('Home', home)
+
+  return (
+    <div>
+      <Button />
+      Hello From React!
+    </div>
+  )
+}
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import shopify from 'vite-plugin-shopify'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [
+    shopify({
+      themeRoot: 'extensions/klaviyo-test-extension'
+    }),
+    react(),
+  ],
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  },
+})
